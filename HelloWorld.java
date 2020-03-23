74a2eb15c7422ef0e61ad0b2314fbc51f2e44730
HighTech <HighTech@NU118>
2020-03-23 15:21:40 +0900

    Creat a Menu

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package helloworld;
-
-public class Helloworld {
-
-	public static void main(String[] args) {
-		System.out.println("Hello, world");
-	}
-}
---
+++ src/Helloworld.java
@@ -0,0 +1,12 @@
+
+public class Helloworld {
+	public static void main(String[] args) {
+		System.out.println("Attendee Management System Menu ***");
+		System.out.println("1. Add Attendee");
+	    System.out.println("2. Delete Attendee");
+	    System.out.println("3. Edit Attendee");
+	    System.out.println("4. View Attendee");
+	    System.out.println("5. Exit");
+	    System.out.println("Select one number between 1 - 5:");
+	}
+}
