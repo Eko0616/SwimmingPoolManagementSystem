21713b1b9015ea241bd26203cfbf2d66eb231bcc
Eko0616 <62534261+Eko0616@users.noreply.github.com>
2020-04-27 12:59:17 -0700

    Update MenuManager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -8,14 +8,15 @@ public class MenuManager {
 	      AttendeeManager attendeeManager = new AttendeeManager(input);
 	      
 	      int num = -1;
-	      while(num != 5) {
+	      while(num != 6) {
 	      System.out.println("*** SwimmingPool Attendee Management System Menu ***");
 	      System.out.println("1. Add Attendee");
 	       System.out.println("2. Delete Attendee");
 	       System.out.println("3. Edit Attendee");
 	       System.out.println("4. View Attendee");
-	       System.out.println("5. Exit");
-	       System.out.println("Select one number between 1 - 5:");
+	       System.out.println("5. Schedule Line");
+	       System.out.println("6. Exit");
+	       System.out.println("Select one number between 1 - 6:");
 	       num = input.nextInt();
 	       if (num == 1) {
 	          attendeeManager.addAttendee();
@@ -29,6 +30,10 @@ public class MenuManager {
 	       else if (num == 4) {
 	          attendeeManager.viewAttendee();
 	           }
+	       else if (num == 5) {
+	    	   attendeeManager.schedulecheck();
+	    	   
+	       }
 	       else {
 	          continue;
 	           }  
