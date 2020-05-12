4416d6e9cb7df36a83e54433741fb93cd8b0d9f3
Eko0616 <62534261+Eko0616@users.noreply.github.com>
2020-05-11 20:49:53 -0700

    Update AttendeeManager.java

--- src/AttendeeManager.java
+++ src/AttendeeManager.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 
 import attendee.Attendee;
 import attendee.AttendeeKind;
+import attendee.AttendeeInput;
 import attendee.ElementarySchoolAttendee;
 import attendee.HighSchoolAttendee;
+import attendee.UniversityAttendee;
 
 public class AttendeeManager {
-	ArrayList<Attendee> attendees = new ArrayList<Attendee>();	
+	ArrayList<AttendeeInput> attendees = new ArrayList<AttendeeInput>();	
 	Scanner input;
 	AttendeeManager(Scanner input) {
 		this.input = input;	
 	}
 	public void addAttendee() {
 		int kind = 0;
-		Attendee attendee;
+		AttendeeInput attendeeInput;
 		while(kind != 1 && kind != 2 && kind != 3 && kind != 4) {
 		System.out.println("1 for University");
 		System.out.println("2 for High School");
@@ -25,27 +27,27 @@ public class AttendeeManager {
 		System.out.print("Select num for Attendee Kind between 1 and 4:");
 		kind = input.nextInt();
 		if (kind == 1) {
-			attendee = new Attendee(AttendeeKind.University);
-			attendee.getUserInput(input);
-			attendees.add(attendee);
+			attendeeInput = new UniversityAttendee(AttendeeKind.University);
+			attendeeInput.getUserInput(input);
+			attendees.add(attendeeInput);
 			break;
 		}
 		else if (kind ==2) {
-			attendee = new HighSchoolAttendee(AttendeeKind.HighSchool);
-			attendee.getUserInput(input);
-			attendees.add(attendee);
+			attendeeInput = new HighSchoolAttendee(AttendeeKind.HighSchool);
+			attendeeInput.getUserInput(input);
+			attendees.add(attendeeInput);
 			break;
 		}
 		else if (kind ==3) {
-			attendee = new HighSchoolAttendee(AttendeeKind.HighSchool);
-			attendee.getUserInput(input);
-			attendees.add(attendee);
+			attendeeInput = new HighSchoolAttendee(AttendeeKind.HighSchool);
+			attendeeInput.getUserInput(input);
+			attendees.add(attendeeInput);
 			break;
 		}
 		else if (kind ==4) {
-			attendee = new ElementarySchoolAttendee(AttendeeKind.ElementarySchool);
-			attendee.getUserInput(input);
-			attendees.add(attendee);
+			attendeeInput = new ElementarySchoolAttendee(AttendeeKind.ElementarySchool);
+			attendeeInput.getUserInput(input);
+			attendees.add(attendeeInput);
 			break;
 		}
 		else {
@@ -57,6 +59,12 @@ public class AttendeeManager {
 	public void deleteAttendee() {
 		System.out.print("Attendee ID:");
 		int attendeeId = input.nextInt();
+		int index = findIndex(attendeeId);
+		removefromAttendees(index, attendeeId);
+					
+	}
+	
+	public int findIndex(int attendeeId) {
 		int index = -1;
 		for (int i = 0; i<attendees.size(); i++) {
 			if (attendees.get(i).getId() == attendeeId) {
@@ -64,61 +72,51 @@ public class AttendeeManager {
 				break;
 			}
 		}
-		
-		if (index >=0) {
+		return index;
+	}
+			
+	public int removefromAttendees(int index, int attendeeId) {
+		if (index >= 0) {
 			attendees.remove(index);
 		System.out.println("the attendee " +attendeeId + " is deleted");
+		return 1;
 		}
 		else {
 			System.out.println("the attendee has not been registered");	
-			return;
-		}			
+			return -1;
+		}
+		
 	}
 	
 	public void editAttendee() {
 		System.out.print("Attendee ID:");
 		int attendeeId = input.nextInt();
 		for (int i = 0; i<attendees.size(); i++) {
-			Attendee attendee = attendees.get(i);
+			AttendeeInput attendee = attendees.get(i);
 		if (attendee.getId() == attendeeId) {
 			int num = -1;
 			while(num != 5) {
-			System.out.println("*** AttendeeInfo Edit Menu ***");
-			System.out.println("1. Edit Id");
-		    System.out.println("2. Edit Name");
-		    System.out.println("3. Edit Email");
-		    System.out.println("4. View Phone");
-		    System.out.println("5. Exit");
-		    System.out.println("Select one number between 1 - 5:");
+				showEditMenu();			
 		    num = input.nextInt();
-		    if (num == 1) {
-		    	System.out.print("Attendee ID:");
-				int id = input.nextInt();
-				attendee.setId(id);
-		        }
-		    else if (num == 2) {
-		    	System.out.print("Attendee name:");
-				String name = input.next();
-				attendee.setName(name);
-		        }
-		    else if (num == 3) {
-		    	System.out.print("Email address:");
-				String email = input.next();
-				attendee.setEmail(email);
-		    }
-		    else if (num == 4) {
-		    	System.out.print("Phone number:");
-				String phone = input.next();
-				attendee.setPhone(phone);
-		        }
-		    else if (num == 5) {
-		    	System.out.print("The group:");
-				String group = input.next();
-				attendee.setGroup(group);
-		    }
-		    else {
+		    switch(num) {
+		    case 1:
+		    	attendee.setAttendeeID(input);
+		    	break;
+		    case 2:
+		    	attendee.setAttendeeName(input);
+		    	break;
+		    case 3:
+		    	attendee.setAttendeeEmail(input);
+		    	break;
+		    case 4:
+		    	attendee.setAttendeePhone(input);
+		    	break;
+		    case 5:
+		    	attendee.setAttendeeGroup(input);
+		    	break;
+		    default:
 		    	continue;
-		         } //if 
+		        }
 			  }	//while
 			break;
 		   } //if
@@ -126,12 +124,10 @@ public class AttendeeManager {
 	}
 	
 	public void viewAttendee() {
-//		System.out.print("Attendee ID:");
-//		int attendeeId = input.nextInt();
 		System.out.println(" # of registered attendees: " + attendees.size());
 		for (int i = 0; i<attendees.size(); i++) {
 			attendees.get(i).printInfo();
-		}
+	   }
 	}
     public void schedulecheck () {
     	System.out.println("스케줄을 확인하세요");
@@ -141,7 +137,19 @@ public class AttendeeManager {
     	System.out.println(" D group (Tue S) 16:30 - 17:50 ");
     	System.out.println(" F group (W F) 15:30 - 16:50 ");
     	System.out.println(" WK group (S) 11:30 - 12:50 , 13:30 - 14:50 ");
-    	
-    	
 	}
-}
+    
+   
+
+
+	public void showEditMenu() {
+		System.out.println("*** AttendeeInfo Edit Menu ***");
+		System.out.println("1. Edit Id");
+	    System.out.println("2. Edit Name");
+	    System.out.println("3. Edit Email");
+	    System.out.println("4. View Phone");
+	    System.out.println("5. Exit");
+	    System.out.println("Select one number between 1 - 5:");
+		
+	  }
+	}
