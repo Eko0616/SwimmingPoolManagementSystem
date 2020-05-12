e09f473c8494152e150b811e214e5bb23db7eb2e
Eko0616 <62534261+Eko0616@users.noreply.github.com>
2020-05-11 20:50:34 -0700

    Update MenuManager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -9,34 +9,38 @@ public class MenuManager {
 	      
 	      int num = -1;
 	      while(num != 6) {
-	      System.out.println("*** SwimmingPool Attendee Management System Menu ***");
-	      System.out.println("1. Add Attendee");
-	       System.out.println("2. Delete Attendee");
-	       System.out.println("3. Edit Attendee");
-	       System.out.println("4. View Attendee");
-	       System.out.println("5. Schedule Line");
-	       System.out.println("6. Exit");
-	       System.out.println("Select one number between 1 - 6:");
+	    	  showMenu();
 	       num = input.nextInt();
-	       if (num == 1) {
-	          attendeeManager.addAttendee();
-	           }
-	       else if (num == 2) {
-	          attendeeManager.deleteAttendee();
-	           }
-	       else if (num == 3) {
-	          attendeeManager.editAttendee();
-	       }
-	       else if (num == 4) {
-	          attendeeManager.viewAttendee();
-	           }
-	       else if (num == 5) {
+	       switch(num) {
+	       case 1:
+	    	   attendeeManager.addAttendee();
+	    	   break;
+	       case 2:
+	    	   attendeeManager.deleteAttendee();
+	    	   break;
+	       case 3:
+	    	   attendeeManager.editAttendee();
+	    	   break;
+	       case 4:
+	    	   attendeeManager.viewAttendee();
+	    	   break;
+	       case 5:
 	    	   attendeeManager.schedulecheck();
-	    	   
-	       }
-	       else {
-	          continue;
+	    	   break;
+	       default:
+	    	   continue;
 	           }  
 	       }
 	  }
+	   
+	   public static void showMenu() {
+		   System.out.println("*** SwimmingPool Attendee Management System Menu ***");
+		      System.out.println("1. Add Attendee");
+		       System.out.println("2. Delete Attendee");
+		       System.out.println("3. Edit Attendee");
+		       System.out.println("4. View Attendee");
+		       System.out.println("5. Schedule Line");
+		       System.out.println("6. Exit");
+		       System.out.println("Select one number between 1 - 6:");  
+	   }
 }
