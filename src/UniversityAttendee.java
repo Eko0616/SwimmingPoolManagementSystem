5d92174ed65c40b614c2c897c1e4db02d3cb3242
Eko0616 <62534261+Eko0616@users.noreply.github.com>
2020-05-11 20:49:11 -0700

    Create UniversityAttendee.java

---
+++ src/UniversityAttendee.java
@@ -0,0 +1,26 @@
+package attendee;
+
+import java.util.Scanner;
+
+public class UniversityAttendee extends Attendee {
+	
+	public UniversityAttendee(AttendeeKind kind) {
+		super(kind);
+	}
+	
+	public void getUserInput(Scanner input) {
+		setAttendeeID(input);
+	 	setAttendeeName(input);
+	 	setAttendeeEmail(input);
+	 	setAttendeePhone(input);
+		setAttendeeGroup(input);
+    }
+	
+	public void printInfo() {
+		String skind = getKindString();	
+		System.out.println(" kind: " + skind + " name: " + name + " id: " + id + " email: " + email + " phone: " + phone + "group: " + group);
+	}
+
+ 
+
+}
