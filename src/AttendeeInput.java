ca5d4c37cf408fa3df0b62a03a7e5c0ae91a5a3c
Eko0616 <62534261+Eko0616@users.noreply.github.com>
2020-05-18 22:18:01 -0700

    Update AttendeeInput.java

--- src/AttendeeInput.java
+++ src/AttendeeInput.java
@@ -1,141 +1,36 @@
 package attendee;
-
 import java.util.Scanner;
 
-public abstract class  Attendee implements AttendeeInput{
-	protected AttendeeKind kind = AttendeeKind.University;
-	protected String name;
-	protected int id;
-	protected String email;
-	protected String phone;
-	protected String group;
-	
-	public Attendee() {
-	}
-	
-	public Attendee(AttendeeKind kind) {
-		this.kind = kind;
-	}
-	
-	public Attendee(String name, int id) {
-		this.name = name;
-		this.id = id;
-	}
-	
-	public Attendee(String name, int id, String email, String phone, String group) {		
-		this.name = name;
-		this.id = id;
-		this.email = email;
-		this.phone = phone;
-		this.group = group;
-	}
-	
-	public Attendee(AttendeeKind kind,String name, int id, String email, String phone, String group) {
-		this.kind = kind;
-		this.name = name;
-		this.id = id;
-		this.email = email;
-		this.phone = phone;
-		this.group = group;
-	}
-	public AttendeeKind getKind() {
-		return kind;
+import exception.EmailFormatException;
+public interface AttendeeInput {
+
+		public int getId();
+		
+		public void setId(int id);
+		
+		public void setEmail(String email) throws EmailFormatException;
+		
+		public void setName(String name);
+		
+		public void setPhone(String phone);
+		
+		public void setGroup(String group);
+		
+		public void getUserInput(Scanner input);
+		
+		public void printInfo();
+
+		public void setAttendeeID(Scanner input);
+		
+		public void setAttendeeName(Scanner input);
+		
+		public void setAttendeeEmail(Scanner input);
+		
+		public void setAttendeePhone(Scanner input);
+		
+		 public void setAttendeeGroup(Scanner input);
+		 
+
 	}
 
-	public void setKind(AttendeeKind kind) {
-		this.kind = kind;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-	
-	public String getGroup() {
-		return group;
-	}
-
-	public void setGroup(String group) {
-		this.group = group;
-	}
-
-	public abstract void printInfo();
-	
-	 public void setAttendeeID(Scanner input) {
-	    	System.out.print("Attendee ID:");
-			int id = input.nextInt();
-			this.setId(id);
-	    }
-	    
-	    public void setAttendeeName(Scanner input) {
-	    	System.out.print("Attendee name:");
-			String name = input.next();
-			this.setName(name);
-	    }
-
-	    public void setAttendeeEmail(Scanner input) {
-	    	System.out.print("Email address:");
-			String email = input.next();
-			this.setEmail(email);
-	    }
-	    
-	    public void setAttendeePhone(Scanner input) {
-	    	System.out.print("Phone number:");
-			String phone = input.next();
-			this.setPhone(phone);
-	    }
-	    
-	    public void setAttendeeGroup(Scanner input) {
-	    	System.out.print("The group:");
-			String group = input.next();
-			this.setGroup(group);
-	    }
-	    
-	    public String getKindString() {
-			String skind = "none";
-			switch(this.kind) {
-			case University:
-				skind = "Univ.";
-				break;
-			case HighSchool:
-				skind = "High";
-				break;
-			case MiddleSchool:
-				skind = "Middle";
-				break;
-			case ElementarySchool:
-				skind = "Elementary";
-				break;
-			default:
-			}
-			return skind;
-		}
-}
 
