ce684179f70f2b59aff41e9165a2e688a4669f06
Eko0616 <62534261+Eko0616@users.noreply.github.com>
2020-05-22 01:05:22 -0700

    Update MenuManager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,14 +1,30 @@
 package jump;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import log.EventLogger;
+
 public class MenuManager {
+	   static EventLogger Logger = new EventLogger("log.txt");
 	   public static void main(String[] args) {
+		  
 	      Scanner input = new Scanner(System.in);
-	      AttendeeManager attendeeManager = new AttendeeManager(input);
-	      
+	      AttendeeManager attendeeManager = getObject("attendeemanager.ser");
+	      if (attendeeManager == null) {
+	    	  attendeeManager = new AttendeeManager(input);
+	      }
+	      else {
+	    	  attendeeManager.setScanner(input);
+	      }
 	     selectMenu(input, attendeeManager);
+	     putObject(attendeeManager, "attendeemanager.ser");
 	  }
 	  
 	  public static void selectMenu(Scanner input, AttendeeManager attendeeManager )  {
@@ -20,18 +36,23 @@ public class MenuManager {
 	       switch(num) {
 	       case 1:
 	    	   attendeeManager.addAttendee();
+	    	   Logger.log("add a attendee");
 	    	   break;
 	       case 2:
 	    	   attendeeManager.deleteAttendee();
+	    	   Logger.log("delete a attendee");
 	    	   break;
 	       case 3:
 	    	   attendeeManager.editAttendee();
+	    	   Logger.log("edit a attendee");
 	    	   break;
 	       case 4:
 	    	   attendeeManager.viewAttendee();
+	    	   Logger.log("view a attendee");
 	    	   break;
 	       case 5:
 	    	   attendeeManager.schedulecheck();
+	    	   Logger.log("schedulecheck");
 	    	   break;
 	       default:
 	    	   continue;
@@ -57,4 +78,44 @@ public class MenuManager {
 		       System.out.println("6. Exit");
 		       System.out.println("Select one number between 1 - 6:");  
 	   }
+	  public static AttendeeManager getObject(String filename) {
+			AttendeeManager attendeeManager = null;
+		try {
+				FileInputStream file = new FileInputStream(filename);
+				ObjectInputStream in = new ObjectInputStream(file);
+				
+				attendeeManager = (AttendeeManager) in.readObject();
+				
+				in.close();
+				file.close();
+		} catch (FileNotFoundException e) {
+				return attendeeManager;
+		} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+		}
+
+			return attendeeManager;
+		}
+
+	public static void putObject(AttendeeManager attendeeManager, String filename) {
+		try {
+			FileOutputStream file = new FileOutputStream(filename);
+			ObjectOutputStream out = new ObjectOutputStream(file);
+			
+			out.writeObject(attendeeManager);
+			
+			out.close();
+			file.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
