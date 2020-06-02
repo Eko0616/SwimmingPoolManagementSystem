1bb589bb277f0af8b0a775bdc9538777db07369b
Eko0616 <62534261+Eko0616@users.noreply.github.com>
2020-06-01 17:04:29 -0700

    Create AttendeeAdder.java

---
+++ src/AttendeeAdder.java
@@ -0,0 +1,60 @@
+package gui;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
+public class AttendeeAdder extends JFrame {
+	
+	public AttendeeAdder() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new SpringLayout());
+		
+		JLabel labelID = new JLabel("ID : ", JLabel.TRAILING);
+		JTextField fieldID = new JTextField(10);
+		labelID.setLabelFor(fieldID);
+		panel.add(labelID);
+		panel.add(fieldID);
+		
+		JLabel labelName= new JLabel("Name : ", JLabel.TRAILING);
+		JTextField fieldName = new JTextField(10);
+		labelName.setLabelFor(fieldName);
+		panel.add(labelName);
+		panel.add(fieldName);
+		
+		JLabel labelEmail= new JLabel("Email : ", JLabel.TRAILING);
+		JTextField fieldEmail = new JTextField(10);
+		labelEmail.setLabelFor(fieldEmail);
+		panel.add(labelEmail);
+		panel.add(fieldEmail);
+		
+		JLabel labelPhone= new JLabel("Phone : ", JLabel.TRAILING);
+		JTextField fieldPhone = new JTextField(10);
+		labelPhone.setLabelFor(fieldPhone);
+		panel.add(labelPhone);
+		panel.add(fieldPhone);
+		
+		JLabel labelGroup= new JLabel("Group : ", JLabel.TRAILING);
+		JTextField fieldGroup = new JTextField(10);
+		labelGroup.setLabelFor(fieldGroup);
+		panel.add(labelGroup);
+		panel.add(fieldGroup);
+		
+		panel.add(new JButton("save"));
+		panel.add(new JButton("cancel"));
+		
+		
+		SpringUtilities.makeCompactGrid(panel, 6, 2, 6, 6, 6, 6);
+		
+		this.setSize(300, 300);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.setContentPane(panel);
+		this.setVisible(true);
+		
+	}
+
+}
