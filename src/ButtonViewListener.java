0e4f50c6193775f1e36ad06ffdf8fd3ea0df7a9d
Eko0616 <62534261+Eko0616@users.noreply.github.com>
2020-06-05 06:57:30 -0700

    Create ButtonViewListener.java

---
+++ src/ButtonViewListener.java
@@ -0,0 +1,26 @@
+package Listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import gui.AttendeeViewer;
+import gui.WindowFrame;
+
+public class ButtonViewListener implements ActionListener {
+	
+	WindowFrame frame;
+
+	public ButtonViewListener(WindowFrame frame) {
+		this.frame = frame;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton b = (JButton) e.getSource();
+		AttendeeViewer viewer = frame.getAttendeeviewer();
+		frame.setupPanel(viewer);
+	}
+
+}
