be73cd1bdc2930a018441da3ebe1ceb332614d1d
Eko0616 <62534261+Eko0616@users.noreply.github.com>
2020-06-15 02:22:20 -0700

    Update AttendeeViewer.java

--- src/AttendeeViewer.java
+++ src/AttendeeViewer.java
@@ -1,17 +1,27 @@
 package gui;
 
+import java.util.Vector;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
+import attendee.AttendeeInput;
+import jump.AttendeeManager;
+
 public class AttendeeViewer extends JPanel{
 	
 	WindowFrame frame;
+	
+	AttendeeManager attendeeManager;
 
-	public AttendeeViewer(WindowFrame frame) {
+	public AttendeeViewer(WindowFrame frame, AttendeeManager attendeeManager) {
 		this.frame = frame;
+		this.attendeeManager = attendeeManager;
+		
+		System.out.println("****" + attendeeManager.size() + "****");
 		
 		DefaultTableModel model = new DefaultTableModel();
 		model.addColumn("Id");
@@ -19,6 +29,17 @@ public class AttendeeViewer extends JPanel{
 		model.addColumn("E-mail");
 		model.addColumn("Contact Info.");
 		model.addColumn("Group");
+		
+		for (int i=0; i< attendeeManager.size(); i++) {
+			Vector row = new Vector();
+			AttendeeInput ai = attendeeManager.get(i);
+			row.add(ai.getId());
+			row.add(ai.getName());
+			row.add(ai.getEmail());
+			row.add(ai.getPhone());
+			row.add(ai.getGroup());
+			model.addRow(row);
+		}
 		
 		JTable table = new JTable(model);
 		JScrollPane sp = new JScrollPane(table);
